b0b3ba4606bd38a4dfbd7727998fba9b32e42f6a
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-01-18 00:26:43 +0000

    Update MariaDB.sql
    missing stables sql

--- MariaDB.sql
+++ MariaDB.sql
@@ -312,7 +312,36 @@ CREATE TABLE `whitelist` (
     ENGINE=InnoDB
     ROW_FORMAT=DYNAMIC;
 
+SET
+  NAMES utf8mb4;
 
+SET
+  FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `stables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `charidentifier` int(11) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `modelname` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `xp` int(11) NULL DEFAULT 0,
+  `injured` int(11) NULL DEFAULT 0,
+  `gear` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `isDefault` int(11) NOT NULL DEFAULT 0,
+  `inventory` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+CREATE TABLE `horse_complements`  (
+  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `charidentifier` int(11) NOT NULL,
+  `complements` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  UNIQUE INDEX `identifier`(`identifier`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 
 INSERT IGNORE INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `metadata`, `desc`) VALUES
